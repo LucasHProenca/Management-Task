e127639e837b68656c542e507897a178575458ad
Lucas <lucasproenca_11@hotmail.com>
2023-01-23 14:52:00 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/managementtask/TaskAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.managementtask
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.managementtask.databinding.RcDaTelaPrincipalBinding
+
+class TaskAdapter(): RecyclerView.Adapter<TaskViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
+
+
+
+
+
+
+
+
+class TaskViewHolder(val binding: RcDaTelaPrincipalBinding): RecyclerView.ViewHolder(binding.root) {
+
+}

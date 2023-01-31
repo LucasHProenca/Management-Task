0f73978c77262183a88f182d88aaad999b7c4754
Lucas <lucasproenca_11@hotmail.com>
2023-01-31 13:59:21 -0300

    Commit Project e seus Fragments

---
+++ app/src/main/java/com/example/managementtask/ProjectDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.managementtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProjectDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_details)
+    }
+}

2cb09fa220e4d79410cab4712d4412135a4b7fb6
Lucas <lucasproenca_11@hotmail.com>
2023-02-02 14:04:42 -0300

    Create Project e Create Task

---
+++ app/src/main/java/com/example/managementtask/TaskDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.managementtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_details)
+    }
+}
